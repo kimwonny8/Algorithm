39be9ad23d00e5137c26ff62a713872fb8678674
KimJungWon <kimwonny8@gmail.com>
2022-08-08 12:21:30 +0900

    백준문제풀이
    20220808

---
+++ Baekjoon/src/Silver/Q7785.java
@@ -0,0 +1,30 @@
+package Silver;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Q7785 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		StringBuilder sb=new StringBuilder();
+		
+		HashMap<String,String> map=new HashMap<>();
+		int n=sc.nextInt();
+		
+		while(n-->0) {
+			String name=sc.next(), state=sc.next();
+			map.put(name, state);
+			if(state.equals("leave")) map.remove(name);
+		}
+		
+		Object[] map2 = map.keySet().toArray(); // 역순으로 정렬하기 위함
+		Arrays.sort(map2, Collections.reverseOrder());
+		
+		for(Object s: map2) {
+			if(map.get(s) != null) sb.append(s+"\n");
+		}
+		System.out.println(sb);
+	}
+}
