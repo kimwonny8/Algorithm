f180a0f968b3d66301a64b4ec8a31862f6c0fb07
KimJungWon <kimwonny8@gmail.com>
2022-08-18 15:44:29 +0900

    20220818

---
+++ Baekjoon/src/Silver/Q2003.java
@@ -0,0 +1,27 @@
+package Silver;
+
+import java.util.Scanner;
+
+public class Q2003 {
+    public static void main(String[] args){
+    	Scanner sc=new Scanner(System.in);
+    	int N=sc.nextInt(), M=sc.nextInt();
+    	int [] arr=new int[N];
+    	for(int i=0; i<N; i++) arr[i]=sc.nextInt();
+    	
+    	int cnt=0, p1=0, p2=0, sum=0;
+    	while(p2<N) {
+			if(sum>M || p2==N) {
+				sum-=arr[p1];
+				p1++;
+			}
+			else {
+				sum+=arr[p2];
+				p2++;
+			}
+         
+			if(sum==M) cnt++;
+		}
+    	System.out.println(cnt);
+    }
+}
