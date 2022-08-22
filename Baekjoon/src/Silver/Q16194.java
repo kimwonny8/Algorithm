3ce7dad78cdd71daf05156afbb3e188a68ace6c9
Jungwon  Kim <86811808+kimwonny8@users.noreply.github.com>
2022-08-22 13:02:55 +0900

    20220822

--- Baekjoon/src/Silver/Q16194.java
+++ Baekjoon/src/Silver/Q16194.java
@@ -7,14 +7,14 @@ public class Q16194 {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		int N=sc.nextInt();
-        int [] arr=new int[N+1];
+        	int [] arr=new int[N+1];
 
-        for(int i=1; i<=N; i++) {
-        	arr[i]=sc.nextInt();
-        	for(int j=1; j<=i; j++) {
-                arr[i]=Math.min(arr[i], arr[i-j]+arr[j]);
-            }
-        }  
-        System.out.println(arr[N]);
+        	for(int i=1; i<=N; i++) {
+        		arr[i]=sc.nextInt();
+        		for(int j=1; j<=i; j++) {
+                	arr[i]=Math.min(arr[i], arr[i-j]+arr[j]);
+            		}
+        	}  
+        	System.out.println(arr[N]);
 	}
 }
