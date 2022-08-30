ddbfe8bbf5ce443dafc7ce0efc5330d65a363e1d
Jungwon  Kim <kimwonny8@gmail.com>
2022-08-30 20:45:33 +0900

    20220830

---
+++ Baekjoon2/src/Bronze/Q4470.java
@@ -0,0 +1,14 @@
+package Bronze;
+
+import java.util.Scanner;
+
+public class Q4470 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int N=Integer.parseInt(sc.nextLine());
+		for(int i=0; i<N; i++) {
+			System.out.println((i+1)+". "+sc.nextLine());
+		}
+	}
+}
