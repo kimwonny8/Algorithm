b830a2ab6c39c210caf2a298ca2a3632a140e061
Jungwon  Kim <kimwonny8@gmail.com>
2022-08-31 16:37:30 +0900

    20220831

---
+++ Baekjoon2/src/Silver/Q25192.java
@@ -0,0 +1,25 @@
+package Silver;
+
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class Q25192 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		TreeSet<String> set=new TreeSet<>();
+		int cnt=0;
+		for(int i=0; i<N; i++) {
+			String str=sc.next();
+			if(str.equals("ENTER")) {
+				cnt+=set.size();
+				set.clear();	
+			}
+			else {
+				set.add(str);
+			}
+			if(i==N-1) cnt+=set.size();
+		}
+		System.out.println(cnt);
+	}
+}
