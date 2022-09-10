b0b0d0b9a44dce817cfd318bd7e9f3610f9611cd
Jungwon  Kim <kimwonny8@gmail.com>
2022-09-10 13:31:37 +0900

    20220910

---
+++ Baekjoon2/src/Silver/Q1057.java
@@ -0,0 +1,19 @@
+package Silver;
+
+import java.util.Scanner;
+
+public class Q1057 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int x=sc.nextInt(), y=sc.nextInt();
+		int cnt=0;
+		while(x!=y) {
+			x=(x/2)+(x%2);
+			y=(y/2)+(y%2);
+			cnt++;
+		}
+		System.out.println(cnt);
+	}
+}
