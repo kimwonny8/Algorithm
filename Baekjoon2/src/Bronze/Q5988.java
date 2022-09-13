b17f4a4f211170704cf5347c366719bc20679401
Jungwon  Kim <kimwonny8@gmail.com>
2022-09-13 09:01:24 +0900

    20220913

---
+++ Baekjoon2/src/Bronze/Q5988.java
@@ -0,0 +1,20 @@
+package Bronze;
+
+import java.util.Scanner;
+
+public class Q5988 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		StringBuilder sb=new StringBuilder();
+		int n=sc.nextInt();
+		if(n!=0) {
+			for(int i=0; i<n; i++) {
+				String[] arr=sc.next().split("");
+				sb.append(Integer.parseInt(arr[arr.length-1])%2==0?"even\n":"odd\n");
+			}
+		}
+		System.out.println(sb);
+	}
+
+}
