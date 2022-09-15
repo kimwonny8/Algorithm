5d15478c22e036b0014f6006599a9123ed7370d2
Jungwon  Kim <kimwonny8@gmail.com>
2022-09-15 17:01:55 +0900

    수업내용추가

---
+++ yju_algorithm/src/yju_algorithm/별찍기.java
@@ -0,0 +1,60 @@
+package yju_algorithm;
+
+import java.util.Scanner;
+
+public class 별찍기 {
+	public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("n을 입력하세요: ");
+		int n=sc.nextInt();
+		for(int i=0; i<n; i++) {
+			System.out.printf("%3d ", i);
+			for(int j=0; j<i+1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		System.out.println("");
+		
+		for(int i=0; i<n; i++) {
+			System.out.printf("%3d ", i);
+			for(int j=0; j<n-i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		
+		for(int i=0; i<n; i++) {
+			System.out.printf("%3d ", i);
+			for(int j=0; j<2*i+1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		System.out.println();
+		
+		for(int i=0; i<n; i++) {
+			System.out.printf("%3d ", i);
+			for(int j=0; j<2*(n-i)-1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		
+		for(int i=0; i<n; i++) {
+			System.out.printf("%3d ", i);
+			for(int j=0; j<n-i; j++) {
+				System.out.print(" ");
+			}
+			for(int j=0; j<i*2+1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	}
+}
+
