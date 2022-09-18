5b5e63103f473c8eebe4d2edf8f1c52a8821c55f
Jungwon  Kim <kimwonny8@gmail.com>
2022-09-18 13:16:40 +0900

    20220918

---
+++ Baekjoon2/src/Bronze/Q14659.java
@@ -0,0 +1,25 @@
+package Bronze;
+
+import java.util.Scanner;
+
+public class Q14659 {
+	public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
+		
+		int n=sc.nextInt();
+		int[] arr=new int[n];
+		for(int i=0; i<n; i++) arr[i]=sc.nextInt();
+		
+		int max=Integer.MIN_VALUE;
+		for(int i=0; i<n; i++) {
+			int cnt=0;
+			for(int j=i+1; j<n; j++) {
+				if(arr[i]>arr[j]) cnt++;
+				else break;
+			}
+			max=Math.max(max, cnt);	
+		}
+		System.out.println(max);
+		
+	}
+}
