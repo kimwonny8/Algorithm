d0ef080f92541e4723a1769f868395be0aeeb993
Jungwon  Kim <kimwonny8@gmail.com>
2022-09-22 17:29:13 +0900

    수업내용 추가

---
+++ yju_algorithm/src/yju_algorithm/배열.java
@@ -0,0 +1,78 @@
+package yju_algorithm;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class 배열 {
+	public static void main(String[] args) {
+//		Scanner sc=new Scanner(System.in);
+		Random rand=new Random();
+		
+		int n=7;
+		int []arr=new int[n];
+		for(int i=0; i<n; i++) {
+			arr[i]=rand.nextInt(100); // 0~100 사이 
+		}
+		System.out.print(Arrays.toString(arr));
+		System.out.println("\n가장 큰 수: "+maxOf(arr));
+		System.out.println("\n요소 순서를 역순으로 출력");
+		reverse(arr);
+		System.out.println("합계: "+sumOf(arr));
+		
+		int []arr2=new int[arr.length];
+		copy(arr, arr2);
+		
+		System.out.println("\n배열 모든 요소를 역순으로 출력");
+		rcopy(arr, arr2);
+	}
+	
+	public static int maxOf(int[] arr) {
+		int max=arr[0];
+		for(int i=0; i<arr.length; i++) {
+			if(arr[i]>max) max=arr[i];
+		}
+			return max;
+	}
+	
+	public static void reverse(int[] arr) {
+		for(int i=0; i<arr.length/2; i++) {
+			int j=arr.length-i-1; // 바꿔야 할 요소
+			System.out.println(Arrays.toString(arr));
+			swap(arr, i, j);
+		}
+		System.out.println("정렬 완료: "+Arrays.toString(arr));
+	}
+	
+	public static void swap(int []arr, int i, int j) {
+		int tmp=arr[i];
+		arr[i]=arr[j];
+		arr[j]=tmp;
+	}
+
+	public static int sumOf(int[] arr) {
+		int sum=0;
+		for(int i=0; i<arr.length; i++) {
+			sum+=arr[i];
+		}
+		return sum;
+	}
+	
+	public static void copy(int []arr, int []arr2) {
+		for(int i=0; i<arr.length; i++) {
+			arr2[i]=arr[i];
+		}
+		System.out.println("arr: "+Arrays.toString(arr));
+		System.out.println("arr2: "+Arrays.toString(arr2));
+	}
+	
+	public static void rcopy(int []arr, int []arr2) {
+		for(int i=0; i<arr.length/2; i++) {
+			int j=arr.length-i-1; // 바꿔야 할 요소
+			swap(arr, i, j);
+			arr2[i]=arr[i];
+		}
+		System.out.println("arr: "+Arrays.toString(arr));
+		System.out.println("arr2: "+Arrays.toString(arr2));
+	}
+
+}
--- yju_algorithm/src/yju_algorithm/별찍기.java
+++ yju_algorithm/src/yju_algorithm/별찍기.java
@@ -7,54 +7,132 @@ public class 별찍기 {
 		Scanner sc=new Scanner(System.in);
 		System.out.print("n을 입력하세요: ");
 		int n=sc.nextInt();
-		for(int i=0; i<n; i++) {
-			System.out.printf("%3d ", i);
-			for(int j=0; j<i+1; j++) {
-				System.out.print("*");
-			}
-			System.out.println();
-		}
-		System.out.println("");
+//		for(int i=0; i<n; i++) {
+//			System.out.printf("%3d ", i);
+//			for(int j=0; j<i+1; j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+//		System.out.println("");
+//		
+//		for(int i=0; i<n; i++) {
+//			System.out.printf("%3d ", i);
+//			for(int j=0; j<n-i; j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+//		System.out.println();
+//		
+//		for(int i=0; i<n; i++) {
+//			System.out.printf("%3d ", i);
+//			for(int j=0; j<2*i+1; j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+//		
+//		System.out.println();
+//		
+//		for(int i=0; i<n; i++) {
+//			System.out.printf("%3d ", i);
+//			for(int j=0; j<2*(n-i)-1; j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+//		System.out.println();
+//		
+//		for(int i=0; i<n; i++) {
+//			System.out.printf("%3d", i);
+//			for(int j=0; j<n-i; j++) {
+//				System.out.print(" ");
+//			}
+//			for(int j=0; j<i*2+1; j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+//		System.out.println();
+//		
+//		for(int i=0; i<n; i++) {
+//			System.out.printf("%3d ", i);
+//			for(int j=0; j<i; j++) {
+//				System.out.print(" ");
+//			}
+//			for(int j=0; j<2*(n-i)-1; j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+//		System.out.println();
+//		
+//		
+//		for(int i=0; i<n; i++) {
+//			System.out.printf("%3d", i);
+//			for(int j=0; j<n-i; j++) {
+//				System.out.print(" ");
+//			}
+//			for(int j=0; j<i*2+1; j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+//		for(int i=0; i<n-1; i++) {
+//			System.out.printf("%3d ", i+n);
+//			for(int j=0; j<i+1; j++) {
+//				System.out.print(" ");
+//			}
+//			for(int j=0; j<2*n-3-(2*i); j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+//		System.out.println();
 		
-		for(int i=0; i<n; i++) {
-			System.out.printf("%3d ", i);
-			for(int j=0; j<n-i; j++) {
-				System.out.print("*");
-			}
-			System.out.println();
-		}
+		System.out.print("몇 개 찍을까용? ");
+		int cnt=sc.nextInt()-1;
 		System.out.println();
 		
-		for(int i=0; i<n; i++) {
-			System.out.printf("%3d ", i);
-			for(int j=0; j<2*i+1; j++) {
-				System.out.print("*");
-			}
-			System.out.println();
-		}
-		
-		System.out.println();
-		
-		for(int i=0; i<n; i++) {
-			System.out.printf("%3d ", i);
-			for(int j=0; j<2*(n-i)-1; j++) {
-				System.out.print("*");
-			}
-			System.out.println();
-		}
-		System.out.println();
-		
-		for(int i=0; i<n; i++) {
-			System.out.printf("%3d ", i);
-			for(int j=0; j<n-i; j++) {
-				System.out.print(" ");
-			}
-			for(int j=0; j<i*2+1; j++) {
-				System.out.print("*");
-			}
-			System.out.println();
-		}
-		
+        for(int i=0; i<n; i++){
+            System.out.printf("%3d ", i);
+            for(int j=0; j<n-i; j++){
+                System.out.print(" ");
+            }
+            for(int j=0; j<2*i+1; j++){
+                System.out.print("*");
+            }
+            
+            for(int k=0; k<cnt; k++) {
+	            for(int j=0; j<n-i-1; j++){
+	                System.out.print("  ");
+	            }
+	            for(int j=0; j<2*i+1; j++){
+	                System.out.print("*");
+	            }
+	        }	
+            System.out.println();
+        }
+        for(int i=0; i<n-1; i++){
+            System.out.printf("%3d  ", n+i);
+            for(int j=0; j<i+1; j++){
+                System.out.print(" ");
+            }
+            for(int j=0; j<2*n-3-2*i; j++){
+                System.out.print("*");
+            }
+            for(int k=0; k<cnt; k++) {
+	            for(int j=0; j<i+1; j++){
+	                System.out.print("  ");
+	            }
+	            for(int j=0; j<2*n-3-2*i; j++){
+	                System.out.print("*");
+	            }
+            }
+            System.out.println();
+        }
+        
 	}
 }
 
