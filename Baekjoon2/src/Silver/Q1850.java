cf65d52dd7e8430090acb2975b153047d67f9d61
Jungwon  Kim <kimwonny8@gmail.com>
2022-09-30 21:29:31 +0900

    20220930

---
+++ Baekjoon2/src/Silver/Q1850.java
@@ -0,0 +1,26 @@
+package Silver;
+
+import java.util.Scanner;
+
+public class Q1850 {
+ 
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in); 
+        StringBuilder sb = new StringBuilder();
+        
+        long a=sc.nextLong(), b=sc.nextLong();
+        long result=gcd(Math.max(a, b), Math.min(a, b));
+        for(int i=0; i<result; i++) sb.append("1");
+        
+        System.out.print(sb);      
+    }
+    
+    static long gcd(long x, long y) {
+        while(y>0) {
+            long tmp=x;
+            x=y;
+            y=tmp%x;
+        }
+        return x;
+    }
+}
