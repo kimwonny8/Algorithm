a9fd3574fa21459fb22b00c6fb3fabc3555d0525
Jungwon  Kim <kimwonny8@gmail.com>
2022-10-22 13:27:32 +0900

    20221022

---
+++ Baekjoon2/src/Silver/Q1699.java
@@ -0,0 +1,23 @@
+package Silver;
+
+import java.util.Scanner;
+
+public class Q1699 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int [] arr=new int[n+1];
+		
+		for(int i=1; i<=n; i++) {
+			arr[i]=i;
+			for(int j=1; j*j<=i; j++) {
+				if(arr[i] > arr[i-j*j]+1) {
+					arr[i]=arr[i-j*j]+1;
+				}
+			}
+		}
+		System.out.println(arr[n]);
+		
+	}
+}
