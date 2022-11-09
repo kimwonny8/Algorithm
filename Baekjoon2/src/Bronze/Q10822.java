1c6fc5a766b96076d1a2a9b9b80e74b9b95a5f92
Jungwon  Kim <kimwonny8@gmail.com>
2022-11-09 14:05:07 +0900

    20221109

---
+++ Baekjoon2/src/Bronze/Q10822.java
@@ -0,0 +1,22 @@
+package Bronze;
+
+import java.util.Scanner;
+
+public class Q10822 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String str=sc.next();
+		String[] arr = str.split(",");
+		
+		int sum=0;
+		
+		for(int i=0; i<arr.length; i++) {
+			int cnt=Integer.parseInt(arr[i]);
+			sum+=cnt;
+		}
+		System.out.println(sum);
+	}
+
+}
