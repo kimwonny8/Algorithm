2d3617733125dc7017be90496ea6506b46fb3648
Jungwon  Kim <kimwonny8@gmail.com>
2022-11-11 09:03:30 +0900

    20221110

---
+++ Baekjoon2/src/Bronze/Q5598.java
@@ -0,0 +1,23 @@
+package Bronze;
+
+import java.util.Scanner;
+
+public class Q5598 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str=sc.next();
+		char [] arr=str.toCharArray();
+		
+		for(int i=0; i<arr.length; i++) {
+			 if(arr[i]-68<0){
+	                System.out.print((char)(arr[i]+23));
+	            }
+	            else{
+	                System.out.print((char)(arr[i]-3));
+	            }
+		}
+	
+	}
+
+}
