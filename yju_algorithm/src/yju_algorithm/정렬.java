ed214762da5b5a57b008f3344032b01dba19e3f0
Jungwon  Kim <kimwonny8@gmail.com>
2022-11-16 15:44:12 +0900

    정렬 수업 내용 추가

--- yju_algorithm/src/yju_algorithm/정렬.java
+++ yju_algorithm/src/yju_algorithm/정렬.java
@@ -1,7 +1,7 @@
 package yju_algorithm;
 
-class BubbleSort {
-	
+class Sorts {
+
 	static void swap(int[] arr, int idx1, int idx2) {
 		int t = arr[idx1];
 		arr[idx1] = arr[idx2];
@@ -9,16 +9,28 @@ class BubbleSort {
 	}
 
 	static void print(int[] arr) {
-		int n=arr.length;
+		int n = arr.length;
 		for (int i = 0; i < n; i++) {
 			System.out.print(arr[i] + " ");
 		}
 	}
-	
+
+	// 배열 출력
+	static void printRow(int[] a, int n) {
+		for (int i = 0; i < n; i++) {
+			System.out.printf("%3d", i);
+		}
+		System.out.println();
+		for (int i = 0; i < n; i++) {
+			System.out.printf("%3d", a[i]);
+		}
+		System.out.println();
+	}
+
 	// 버블 정렬
-	static void bubbleSort(int[] arr) {
-		int n=arr.length;
-		
+	static void Sorts(int[] arr) {
+		int n = arr.length;
+
 		for (int i = 0; i < n - 1; i++) {
 			for (int j = n - 1; j > i; j--) {
 				if (arr[j - 1] > arr[j]) {
@@ -29,9 +41,9 @@ class BubbleSort {
 	}
 
 	// 버블정렬_연습문제
-	static void bubbleSort2(int[] arr) {
-		int n=arr.length;
-		
+	static void Sorts2(int[] arr) {
+		int n = arr.length;
+
 		for (int i = 0; i < n - 1; i++) {
 			System.out.printf("패스%d :\n", i + 1);
 
@@ -50,11 +62,11 @@ class BubbleSort {
 			System.out.println();
 		}
 	}
-	
+
 	// 버블 정렬 - 교환이 더 이상 일어나지 않으면 멈춤
-	static void bubbleSort3(int[] arr) {
-		int n=arr.length;
-		
+	static void Sorts3(int[] arr) {
+		int n = arr.length;
+
 		int wcnt = 0; // 전체 교환 횟수
 		for (int i = 0; i < n - 1; i++) {
 			int cnt = 0;
@@ -72,56 +84,148 @@ class BubbleSort {
 		System.out.println("전체교환횟수: " + wcnt);
 		print(arr);
 	}
-	
+
 	// 단순 선택 정렬 - 제일 작은 값 찾아서 맨 앞으로 이동
-	static void selectionSort(int []arr) {
-		int n=arr.length;
-		
-		for(int i=0; i<n-1; i++) {
-			int min=i;
+	static void selectionSort(int[] arr) {
+		int n = arr.length;
+
+		for (int i = 0; i < n - 1; i++) {
+			int min = i;
 			print(arr);
-			for(int j=i+1; j<n; j++) {
-				if(arr[j]<arr[min]) {
-					min=j;
+			for (int j = i + 1; j < n; j++) {
+				if (arr[j] < arr[min]) {
+					min = j;
 				}
 			}
-			System.out.printf("  가장 작은 값:%d, 위치:%d\n",arr[min], min);
-			swap(arr,i,min);
+			System.out.printf("  가장 작은 값:%d, 위치:%d\n", arr[min], min);
+			swap(arr, i, min);
 		}
 		System.out.print("\n정렬 완료: ");
 		print(arr);
 	}
 
 	// 단순 삽입 정렬 - 두번째부터, 그 값이 앞보다 작으면 앞을 뒤로 밀고 작은 수를 앞으로
-	static void insertionSort(int []arr) {
-		int n=arr.length;
-		for(int i=1; i<n; i++) {
-			int tmp=arr[i];
-			int idx=0; // 들어갈 자리 찾기
+	static void insertionSort(int[] arr) {
+		int n = arr.length;
 
-			for(int j=0; j<i; j++) {
-				if(arr[j] < tmp) continue;
-				else break;
+		for (int i = 1; i < n; i++) {
+			int tmp = arr[i];
+			int j;
+
+			for (j = 0; j < i; j++) {
+				if (arr[j] < tmp)
+					continue;
+				else
+					break;
 			}
-			
+
 			// 들어갈 위치 뒤에꺼 한 칸씩 밀기
-			for(int j=i; j>idx; j--) {
-				arr[j]=arr[j-1];
+			for (int k = i; k >= j + 1; k--) {
+				arr[k] = arr[k - 1];
 			}
+			arr[j] = tmp;
+
 			print(arr);
 			System.out.println();
 		}
+
+	}
+
+	// 단순 삽입 정렬2 - 중간 과정이 나오도록 프로그램을 수정
+	static void insertionSort2(int[] arr) {
+		int n = arr.length;
+
+		for (int i = 1; i < n; i++) {
+			System.out.printf("처리 arr[%d]=[%d]  삽입될 위치: ", i, arr[i]);
+			int tmp = arr[i];
+			int j;
+
+			for (j = 0; j < i; j++) {
+				if (arr[j] < tmp)
+					continue;
+				else
+					break;
+			}
+
+			System.out.println(j);
+			printRow(arr, arr.length);
+			// 들어갈 위치 뒤에꺼 한 칸씩 밀기
+			for (int k = i; k >= j + 1; k--) {
+				arr[k] = arr[k - 1];
+			}
+			arr[j] = tmp;
+		}
+
+	}
+
+	// 셸 정렬 => 8개면 4칸, 2칸, 1칸 순으로 차이나는 값 비교해서 교체(0번과 4번 인덱스)
+	static void shellSort(int[] arr) {
+		int n = arr.length;
+
+		for (int h = n / 2; h > 0; h /= 2) {
+			for (int i = h; i < n; i++) {
+				int j;
+				int tmp = arr[i];
+				for (j = i - h; j >= 0 && arr[j] > tmp; j -= h) {
+					arr[j + h] = arr[j];
+				}
+				arr[j + h] = tmp;
+			}
+		}
+	}
+
+	// 셸 정렬2 - h값이 서로 배수가 되지 않도록 만듦
+	static void shellSort2(int[] arr) {
+		int n = arr.length;
+
+		int h;
+		for (h = 1; h < n; h = h * 3 + 1) // +1은 나눗셈하면 어차피 날아감
+			;
+
+		for (; h > 0; h /= 3) {
+			for (int i = h; i < n; i++) {
+				int j;
+				int tmp = arr[i];
+				for (j = i - h; j >= 0 && arr[j] > tmp; j -= h) {
+					arr[j + h] = arr[j];
+				}
+				arr[j + h] = tmp;
+			}
+		}
+	}
+	
+	// 퀵 정렬 => O(n log n), 최악일 경우 O(n2)
+	static void quickSort(int []arr, int left, int right) {
+		int pl = left; // 왼쪽 포인터
+		int pr = right; // 오른쪽 포인터
+		int x = arr[(pl+pr)/2]; // 피벗(가운데)
 		
+		do {
+			while(arr[pl] < x) pl++;
+			while(arr[pr] > x) pr--;
+			if(pl <= pr) swap(arr, pl++, pr--);
+			
+		} while(pl<=pr); // 왼쪽이 오른쪽을 넘을 때 끝
+		
+		if(left < pr) quickSort(arr, left, pr);
+		if(pl < right) quickSort(arr, pl, right);
 	}
 }
 
 public class 정렬 {
 	public static void main(String[] args) {
 		int[] arr = { 7, 6, 4, 3, 1, 9, 8 };
-		int[] arr2 = { 6,4,1,7,3,9,8 };
-		// BubbleSort.bubbleSort2(arr);
-		//BubbleSort.bubbleSort3(arr2);
-		//BubbleSort.selectionSort(arr);
-		BubbleSort.insertionSort(arr2);
+		int[] arr2 = { 6, 4, 8, 3, 1, 9, 7 };
+		// Sorts.Sorts2(arr);
+		// Sorts.Sorts3(arr2);
+		// Sorts.selectionSort(arr);
+		// Sorts.insertionSort(arr2);
+		// Sorts.insertionSort2(arr2);
+		// Sorts.shellSort(arr);
+		// Sorts.shellSort2(arr);
+				
+		int [] x = {5,7,1,4,6,2,3,9,8};
+		Sorts.quickSort(x, 0, x.length-1);
+		Sorts.print(x);
 	}
 }
